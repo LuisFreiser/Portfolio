ab98c25c82f1de604fe3aaedb517e1426140fc7b
Luis Suarez <luisfreiser@gmail.com>
2024-11-19 17:10:16 -0500

    second commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,9 @@ const footer = () => {
     <footer className="max-w-3xl mx-auto">
       <div className="md:flex md:justify-between">
         <div>
-          <h4 className="text-3xl font-bold my-6 md:my-0">S&V Desing Studio</h4>
+          <h4 className="text-3xl font-bold my-6 text-center md:my-0">
+            S&V Desing Studio
+          </h4>
         </div>
         <div className="px-2 flex justify-between md:gap-8 items-center">
           <Link href="#about-me">Sobre Mi</Link>
